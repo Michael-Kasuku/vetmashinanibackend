d4204dfd945f8a208ac0892657ecb0eea47862b4
Michael Kasuku <michaelotienokasuku@gmail.com>
2025-04-10 11:54:06 +0300

    Updated

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import predict_disease
-from .views import job_vibe
 
 urlpatterns = [
-    path("predict/", predict_disease, name="predict_disease"),
-    path("jobvibe/", job_vibe, name="job_vibe"),
+    path("predict/disease", predict_disease, name="predict_disease"),
 ]
